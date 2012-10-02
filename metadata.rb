39ad0fd130538d3ca09f1258b85088a41601c89d
Robert Allen <zircote@gmail.com>
2012-10-01 22:33:07 -0500

    updated metadata.rb and generated metadata.json

--- metadata.rb
+++ metadata.rb
@@ -21,6 +21,13 @@
 maintainer       "Robert Allen"
 maintainer_email "zircote@gmail.com"
 license          "Apache 2.0"
-description      "Installs/Configures composer"
+description      "Installs/Configures php composer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+%w{ debian ubuntu centos redhat fedora scientific amazon suse }.each do |os|
+  supports os
+end
+
+recipe "composer", "PHP Composer Installation and project interactions"
+
