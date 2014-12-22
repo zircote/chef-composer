cb27eb8246510541f9493b568050baaf3aa961df
driesdroesbeke <dries.droesbeke@gmail.com>
2014-12-22 18:05:18 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -25,6 +25,7 @@ license          "Apache 2.0"
 description      "Installs/Configures php composer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+name             "composer"
 
 %w{ debian ubuntu centos redhat fedora scientific amazon suse }.each do |os|
   supports os
